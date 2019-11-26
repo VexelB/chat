0dfc0c304c5d1939cad36800f33082b9ae05a7fc
Voytin Evgeny <vexelb@gmail.com>
2019-11-26 14:26:17 +0200

    Add files via upload

---
+++ precache-manifest.3acff0fbe4d9d56594ef71156bad264b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b1d58e54e3745b7d6fc2ccdcc5a6bb4",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "b042844b3a103b369525",
+    "url": "/chat/static/css/main.70129986.chunk.css"
+  },
+  {
+    "revision": "54867696e34558b3ddb5",
+    "url": "/chat/static/js/2.96867f49.chunk.js"
+  },
+  {
+    "revision": "b042844b3a103b369525",
+    "url": "/chat/static/js/main.bc3eff04.chunk.js"
+  },
+  {
+    "revision": "8b3c7a39cdeaacd69708",
+    "url": "/chat/static/js/runtime-main.e89090b1.js"
+  }
+]);
